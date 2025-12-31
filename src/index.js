2cb904a1e81569ab711ce85a4817c841530781c4
ALPHA2204 <thisisalok1334@gmail.com>
2025-12-31 14:40:29 +0530

    Middlewares SetUp

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import dotenv from 'dotenv';
 import  connectDB  from "./db/index.js";
-
+import app from "./app.js"
 // Error :: import  {CONNECTDB} from "./db/index.js because  you export 1 thing --> by name connectDB "
 
 dotenv.config() // to get all the env variable before starting the app
 
 connectDB()
+.then(()=>{
+    // We are going for the error in the app before listen the app
+    app.on("error",(error)=>{
+        console.log("ERROR:",error);
+        throw error
+        process.exit(1)
+    })
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(` Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!",err);
+})
+
+
 
 /*
 import express from "express";
